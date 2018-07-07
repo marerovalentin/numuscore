0031b049c2b467043b5cc95147f65d2c825e6e84
numuscrypto <34494166+numuscrypto@users.noreply.github.com>
2018-07-07 06:27:05 +0200

    typo

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -1,4 +1,4 @@
-96dbf7bcd01c2d97621e8e9a7a23afc48ddc37ecba68a697f1890ad95f5a253a// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2010 Satoshi Nakamoto
 // Copyright (c) 2009-2012 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
